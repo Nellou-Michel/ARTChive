5de0e1196d639e09a39afcaf585f17b616f89427
Maëlis-Rakowski <maelis.rakowski@etu.umontpellier.fr>
2023-09-21 16:31:50 +0200

    first trigger

---
+++ demo-postgresql-php/sql/procedures.sql
@@ -0,0 +1,54 @@
+CREATE TRIGGER check_media_genre_category 
+BEFORE INSERT OR UPDATE ON GenreMedia 
+FOR EACH ROW
+declare
+    media_type VARCHAR(255);
+    category_genre VARCHR(255);
+BEGIN
+    -- Obtenir le type du média associé
+    SELECT
+        CASE
+            WHEN EXISTS (SELECT 1 FROM Book WHERE id_media = :NEW.id_media) THEN 'Book'
+            WHEN EXISTS (SELECT 1 FROM Movie WHERE id_media = :NEW.id_media) THEN 'Movie'
+            WHEN EXISTS (SELECT 1 FROM Game WHERE id_media = :NEW.id_media) THEN 'Game'
+            ELSE NULL
+        END
+    INTO media_type
+    FROM dual;
+
+    -- Obtenir la category du genre
+    SELECT category INTO category_genre
+    FROM Genre 
+    WHERE id_genre = :NEW.id_genre;
+
+    -- Vérifier si le genre correspond à la catégorie du média
+    IF category IS NOT NULL THEN 
+        IF media_type IS NOT NULL AND :NEW.category != media_type THEN
+            RAISE_APPLICATION_ERROR(-20001, 'Le genre ne correspond pas à la catégorie du média.');
+        END IF;
+        EXCEPTION
+            WHEN NO_DATA_FOUND THEN
+                RAISE_APPLICATION_ERROR(-20002, 'Média introuvable.');
+            WHEN OTHERS THEN
+                RAISE;
+    END IF;
+    EXCEPTION
+        WHEN NO_DATA_FOUND THEN
+            RAISE_APPLICATION_ERROR(-20002, 'Genre introuvable.');
+        WHEN OTHERS THEN
+            RAISE;
+END;
+
+
+
+CREATE OR REPLACE FUNCTION FUNCTION VerifyMediaGenreCategory
+    si media est book
+        si le genre  est de category book
+                ok
+    si media est movie
+        si le genre  est de category Movie
+                    ok
+    si media est Game
+        si le genre est de category Game 
+
+INSERT INTO Book VALUES(id_media, 'Roman')
