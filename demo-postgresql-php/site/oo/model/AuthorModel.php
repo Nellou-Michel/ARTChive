2349e5585e98e2997e68f6d4078249393619c5ee
logandej <74404279+logandej@users.noreply.github.com>
2023-11-09 08:56:21 +0100

    auteurs dans média

---
+++ demo-postgresql-php/site/oo/model/AuthorModel.php
@@ -0,0 +1,65 @@
+<?php
+require_once File::build_path(array("model","Model.php"));//Model.php
+
+class AuthorModel extends Model{
+
+    private $id_author;
+    private $name_author;
+
+    public function __construct(array $data) {
+        $this->hydrate($data);
+    }
+
+    private function setId_author($id_author){
+        $this->id_author=$id_author;
+    }
+
+    private function setName_author($name_author){
+        $this->name_author=$name_author;
+    }
+
+    public function getId_author(){
+        return $this->id_author;
+    } 
+
+    public function getName_author(){
+        return $this->name_author;
+    } 
+
+    public function hydrate(array $data){
+        foreach($data as $key => $value){
+            $method = 'set'.ucfirst($key);
+            if(method_exists($this, $method)){
+                $this->$method($value);
+            }
+            else{
+                echo "[methode introuvable : ". $method."]__";
+            }
+        }
+    }
+
+
+    public static function getAllAuthors(){
+        echo"getall";
+         $var = [];
+         $req =  DB::get()->query('SELECT name_author FROM Author');
+         $req->execute();
+         while($data = $req->fetch(PDO::FETCH_ASSOC)){
+             $var[] = new AuthorModel($data);
+            
+         }
+         return $var;
+         $req->closeCursor();
+       
+         
+    }
+
+    public static function addAuthor($name) {
+        $req = DB::get()->prepare("INSERT INTO Author (name_author) VALUES (:name_author)");
+        $req->bindParam(':name_author', $name, PDO::PARAM_STR);
+        $req->execute();
+    }
+
+
+
+}
